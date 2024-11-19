2c7cbc0ecd52f089640810ce62846293191d3d9f
innovator-zero <2529300178@qq.com>
2024-11-18 21:27:18 -0600

    Modify loss for DINOv2

--- depth/models/losses/sigloss.py
+++ depth/models/losses/sigloss.py
@@ -1,14 +1,19 @@
-# Copyright (c) OpenMMLab. All rights reserved.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the Apache License, Version 2.0
+# found in the LICENSE file in the root directory of this source tree.
+
 import torch
 import torch.nn as nn
 
-from depth.models.builder import LOSSES
+from ...models.builder import LOSSES
+
 
 @LOSSES.register_module()
 class SigLoss(nn.Module):
     """SigLoss.
 
-        We adopt the implementation in `Adabins <https://github.com/shariqfarooq123/AdaBins/blob/main/loss.py>`_.
+        This follows `AdaBins <https://arxiv.org/abs/2011.14141>`_.
 
     Args:
         valid_mask (bool): Whether filter invalid gt (gt > 0). Default: True.
@@ -18,18 +23,16 @@ class SigLoss(nn.Module):
         warm_iter (int): The number of warm up stage. Default: 100.
     """
 
-    def __init__(self,
-                 valid_mask=True,
-                 loss_weight=1.0,
-                 max_depth=None,
-                 warm_up=False,
-                 warm_iter=100):
+    def __init__(
+        self, valid_mask=True, loss_weight=1.0, max_depth=None, warm_up=False, warm_iter=100, loss_name="sigloss"
+    ):
         super(SigLoss, self).__init__()
         self.valid_mask = valid_mask
         self.loss_weight = loss_weight
         self.max_depth = max_depth
+        self.loss_name = loss_name
 
-        self.eps = 0.001 # avoid grad explode
+        self.eps = 0.001  # avoid grad explode
 
         # HACK: a hack implementation for warmup sigloss
         self.warm_up = warm_up
@@ -43,7 +46,7 @@ class SigLoss(nn.Module):
                 valid_mask = torch.logical_and(target > 0, target <= self.max_depth)
             input = input[valid_mask]
             target = target[valid_mask]
-        
+
         if self.warm_up:
             if self.warm_up_counter < self.warm_iter:
                 g = torch.log(input + self.eps) - torch.log(target + self.eps)
@@ -57,6 +60,6 @@ class SigLoss(nn.Module):
 
     def forward(self, depth_pred, depth_gt):
         """Forward function."""
-        
+
         loss_depth = self.loss_weight * self.sigloss(depth_pred, depth_gt)
         return loss_depth
